5ef9343a6d508e1f7dd1904448c0b0ed3f3436e0
Charlie Garrett <garrett@concurix.com>
2013-02-15 09:46:03 -0800

    Remove common functions from boss_router_controller.erl

--- src/boss/boss_router.erl
+++ src/boss/boss_router.erl
@@ -9,22 +9,12 @@
 -export([start/0, start/1, stop/0]).
 -export([reload/1, route/2, unroute/4, handle/2, get_all/1, set_controllers/2]).
 
+-include("boss_router.hrl").
 
 %% Maintain routing and handler state in ETS tables rather than a
 %% boss_router_controller process.
 -define(BOSS_ROUTES_TABLE, boss_routes).
 -define(BOSS_HANDLERS_TABLE, boss_handlers).
-
--record(boss_route, {number, url, pattern, application, controller, action, params = []}).
-
--record(boss_handler, {status_code, application, controller, action, params = []}).
-
--record(boss_router_config, {
-          application,
-          controllers = [],
-          routes_table_id,
-          handlers_table_id
-         }).
 
 %%
 %% API Functions
@@ -38,8 +28,8 @@ start(Options) ->
     Controllers = proplists:get_value(controllers, Options, []),
     RoutesTableId = ets:new(?BOSS_ROUTES_TABLE, [ordered_set, public, {keypos, 2}]),
     HandlersTableId = ets:new(?BOSS_HANDLERS_TABLE, [ordered_set, public, {keypos, 2}]),
-    Config = #boss_router_config{ application = BossApp, routes_table_id = RoutesTableId, 
-                                  handlers_table_id = HandlersTableId, controllers = Controllers },
+    Config = #state{ application = BossApp, routes_table_id = RoutesTableId, 
+                     handlers_table_id = HandlersTableId, controllers = Controllers },
     load(Config),
     {ok, Config}.
 
@@ -49,15 +39,15 @@ stop() ->
 %% reload modifies the tables within a configuration, but does not need to
 %% return a new configuration because tables are stateful.
 reload(Config) ->
-    ets:delete_all_objects(Config#boss_router_config.routes_table_id),
-    ets:delete_all_objects(Config#boss_router_config.handlers_table_id),
+    ets:delete_all_objects(Config#state.routes_table_id),
+    ets:delete_all_objects(Config#state.handlers_table_id),
     load(Config),
     ok.
 
 route(Config, "") ->
     route(Config, "/");
-route(#boss_router_config{application = AppName, routes_table_id = RoutesTable}=Config, Url) ->
-    Route = case get_match(Url, ets:tab2list(RoutesTable)) of
+route(#state{application = AppName, routes_table_id = RoutesTable}=Config, Url) ->
+    case get_match(Url, ets:tab2list(RoutesTable)) of
         undefined -> 
             case string:tokens(Url, "/") of
                 [Controller] -> 
@@ -77,13 +67,11 @@ route(#boss_router_config{application = AppName, routes_table_id = RoutesTable}=
             ControllerModule = list_to_atom(boss_files:web_controller(App, C)),
             {Tokens, []} = boss_controller_lib:convert_params_to_tokens(P, ControllerModule, list_to_atom(A)),
             {ok, {App, C, A, Tokens}}
-    end,
-    Route.
-
+    end.
 
 unroute(Config, Controller, undefined, Params) ->
     unroute(Config, Controller, default_action(Config, Controller), Params);
-unroute(#boss_router_config{application = AppName, routes_table_id = RoutesTable}, Controller, Action, Params) ->
+unroute(#state{application = AppName, routes_table_id = RoutesTable}, Controller, Action, Params) ->
     RoutedURL = ets:foldl(fun
             (#boss_route{ application = App, controller = C, action = A, params = P } = Route, Default) 
                 when App =:= AppName, C =:= Controller, A =:= Action ->
@@ -96,7 +84,7 @@ unroute(#boss_router_config{application = AppName, routes_table_id = RoutesTable
             (_, Default) ->
                 Default
         end, undefined, RoutesTable),
-    Result = case RoutedURL of
+    case RoutedURL of
         undefined ->
             ControllerModule = list_to_atom(boss_files:web_controller(AppName, Controller)),
             {Tokens, Variables1} = boss_controller_lib:convert_params_to_tokens(Params, ControllerModule, list_to_atom(Action)),
@@ -117,11 +105,10 @@ unroute(#boss_router_config{application = AppName, routes_table_id = RoutesTable
             end;
         _ ->
             RoutedURL
-    end,
-    Result.
+    end.
 
 handle(Config, StatusCode) ->
-    case ets:lookup(Config#boss_router_config.handlers_table_id, StatusCode) of
+    case ets:lookup(Config#state.handlers_table_id, StatusCode) of
         [] ->
             not_found;
         [#boss_handler{ application = App, controller = C, action = A, params = P }] ->
@@ -134,26 +121,24 @@ get_all(Config) ->
     lists:map(
       fun(#boss_route{ url = U, application = App, controller = C, action = A, params = P }) -> 
               [{url, U}, {application, App}, {controller, C}, {action, A}, {params, P}]
-      end, lists:flatten(ets:match(Config#boss_router_config.routes_table_id, '$1'))).
+      end, lists:flatten(ets:match(Config#state.routes_table_id, '$1'))).
 
 %% set_controllers returns a modified configuration, which should be retained
 %% by the caller.
 set_controllers(Config, ControllerList) ->
-    Config#boss_router_config{ controllers = ControllerList }.
-
+    Config#state{ controllers = ControllerList }.
 
 %% Internal functions
 
-load(#boss_router_config{application = AppName, routes_table_id = RoutesTable,
-                         handlers_table_id = HandlersTable}) ->
-    RoutesFile = boss_files:routes_file(AppName),
+load(State) ->
+    RoutesFile = boss_files:routes_file(State#state.application),
     error_logger:info_msg("Loading routes from ~p ....~n", [RoutesFile]),
     case file:consult(RoutesFile) of
         {ok, OrderedRoutes} -> 
             Routes = lists:zipwith(fun(Number, {Url, Proplist}) -> {Number, Url, Proplist} end, lists:seq(1,length(OrderedRoutes)), OrderedRoutes),
             lists:map(fun
                     ({Number, UrlOrStatusCode, Proplist}) when is_list(Proplist) ->
-                        TheApplication = proplists:get_value(application, Proplist, AppName),
+                        TheApplication = proplists:get_value(application, Proplist, State#state.application),
                         TheController = proplists:get_value(controller, Proplist),
                         TheAction = proplists:get_value(action, Proplist),
                         CleanParams = lists:foldl(fun(Key, Vars) ->
@@ -170,7 +155,7 @@ load(#boss_router_config{application = AppName, routes_table_id = RoutesTable,
                                     controller = TheController, 
                                     action = TheAction, 
                                     params = CleanParams },
-                                true = ets:insert(RoutesTable, NewRoute);
+                                true = ets:insert(State#state.routes_table_id, NewRoute);
                             StatusCode when is_integer(StatusCode) ->
                                 NewHandler = #boss_handler{ 
                                     status_code = StatusCode, 
@@ -178,20 +163,20 @@ load(#boss_router_config{application = AppName, routes_table_id = RoutesTable,
                                     controller = TheController,
                                     action = TheAction, 
                                     params = CleanParams },
-                                true = ets:insert(HandlersTable, NewHandler)
+                                true = ets:insert(State#state.handlers_table_id, NewHandler)
                         end
                 end, Routes);
         Error -> 
             error_logger:error_msg("Missing or invalid boss.routes file in ~p~n~p~n", [RoutesFile, Error])
     end.
 
-is_controller(Config, Controller) -> 
-    lists:member(boss_files:web_controller(Config#boss_router_config.application, Controller), Config#boss_router_config.controllers).
+is_controller(State, Controller) -> 
+    lists:member(boss_files:web_controller(State#state.application, Controller), State#state.controllers).
 
-default_action(Config, Controller) ->
-    case is_controller(Config, Controller) of
+default_action(State, Controller) ->
+    case is_controller(State, Controller) of
         true ->
-            ControllerModule = list_to_atom(boss_files:web_controller(Config#boss_router_config.application, Controller)),
+            ControllerModule = list_to_atom(boss_files:web_controller(State#state.application, Controller)),
             case proplists:get_value(default_action, ControllerModule:module_info(attributes)) of
                 [DefaultAction] when is_atom(DefaultAction) ->
                     atom_to_list(DefaultAction);
